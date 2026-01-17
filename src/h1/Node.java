20559c01aef68e634678345b0bae7907ef376a63
Maxi21d <maxidecaro650@gmail.com>
2026-01-17 01:18:23 +0100

    H1

---
+++ src/h1/Node.java
@@ -0,0 +1,18 @@
+package h1;
+
+public class Node {
+    private Node next;
+    public Node(Node next) {
+        setNext(next);
+    }
+
+    // Setter:
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    // Getter:
+    public Node getNext() {
+        return next;
+    }
+
+}
